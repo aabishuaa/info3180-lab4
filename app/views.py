3bcee86c44185d15e19ac1d056d760e86180bbbf
aabishuaa <abishuasjohnson@gmail.com>
2025-03-10 22:26:55 -0500

    C:/Program Files/Git/login route updated

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
-
+from werkzeug.security import check_password_hash
 
 ###
 # Routing for your application.
@@ -36,27 +36,25 @@ def upload():
 
     return render_template('upload.html')
 
-
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
+    
+    if form.validate_on_submit():  
+        username = form.username.data
+        password = form.password.data
 
-    # change this to actually validate the entire form submission
-    # and not just one field
-    if form.username.data:
-        # Get the username and password values from the form.
+        
+        user = UserProfile.query.filter_by(username=username).first()
 
-        # Using your model, query database for a user based on the username
-        # and password submitted. Remember you need to compare the password hash.
-        # You will need to import the appropriate function to do so.
-        # Then store the result of that query to a `user` variable so it can be
-        # passed to the login_user() method below.
+        if user and check_password_hash(user.password, password):  
+            login_user(user)
+            flash("Login successful!", "success")
+            return redirect(url_for("upload"))  # Redirect to upload page
 
-        # Gets user id, load into session
-        login_user(user)
+        else:
+            flash("Invalid username or password. Please try again.", "danger")
 
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
